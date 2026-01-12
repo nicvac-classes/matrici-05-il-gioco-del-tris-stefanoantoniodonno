545cc2af643cb1551f6165b03db511cb3c5081e1
stefanoantoniodonno <stefanoantonio.donno@marconibari.edu.it>
2026-01-12 20:12:49 +0000

    es tris

--- Esercizio.java
+++ Esercizio.java
@@ -1,33 +1,50 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
 import java.util.Scanner;
 
-// Classe principale, con metodo main
 class Esercizio {
 
-    //Input da tastiera
     static Scanner input = new Scanner(System.in);
-    
-    //Inserisce il simbolo x oppure o nella grigla di gioco in riga i e colonna j.
-    //Se la mossa non è valida (pedina già presente o le coordinate sono fuori la griglia) allora ritorno falso.
-    static boolean inserisciInGriglia( /*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
+
+    static boolean inserisciInGriglia(String[][] board, int i, int j, String s) {
+        boolean ok = false;
+        --i;
+        --j;
+        
+        ok = (0 <= i && i <= 2 && 0 <= j && j <= 2 && board[i][j].equals("-"));
+
+        if(ok){
+            board[i][j] = "s";
+        }
+
+        return ok;
     }
 
-    //Azzero la griglia di gioco inserendo la stringa "-" in tutte le celle.
-    static void azzeraGriglia(/*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
+    static void azzeraGriglia(String[][] board, int r, int c) {
+        for(int i = 0; i < r; ++i){
+            for(int j = 0; j < c; ++j){
+                board[i][j] = "-";
+            }
+       }
     }
 
-    //Controlla se nella griglia c'è una vincita.
-    // s può valore "O" oppure "X"
-    static boolean controllaVincita(/*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
+    static boolean controllaVincita(String[][] board, String s) {
+        boolean ok = false;
+        ok = (
+                (board[0][0].equals(s) && board[0][1].equals(s) && board[0][2].equals(s)) || 
+                (board[1][0].equals(s) && board[1][1].equals(s) && board[1][2].equals(s)) ||
+                (board[2][0].equals(s) && board[2][1].equals(s) && board[2][2].equals(s)) ||
+                (board[0][0].equals(s) && board[1][0].equals(s) && board[2][0].equals(s)) ||
+                (board[0][1].equals(s) && board[1][1].equals(s) && board[2][1].equals(s)) ||
+                (board[0][2].equals(s) && board[1][2].equals(s) && board[2][2].equals(s)) ||
+                (board[3][0].equals(s) && board[1][1].equals(s) && board[0][2].equals(s)) ||
+                (board[0][0].equals(s) && board[1][1].equals(s) && board[2][2].equals(s))
+            );
+
+        return ok;
     }
 
     //Conta quante caselle libere ci sono ancora.
     //Se non ci sono caselle libere e non si è vinto allora è un pareggio
-    static int contaCaselleLibere(/*scrivere qui i parametri richiesti dall'esercizio*/ ) {
+    static int contaCaselleLibere(/scrivere qui i parametri richiesti dall'esercizio/ ) {
         //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
     }
 
